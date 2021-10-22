d8e5236d1db9e62ba0a6551654f99d266948ca67
yusuf kukus <yusufkukus@gmail.com>
2021-10-22 19:41:14 +0300

    ejs eklendi

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine',"ejs");
+app.use(express.static('public'));
+ 
 app.get("/", (req, res) => {
-  res.send("Indexx sayfası");
+  res.status(200).render("index",{
+    page_name:"index"
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about",{
+    page_name:"about",
+  });
 });
 
 const port = 3000;
